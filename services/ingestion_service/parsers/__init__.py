65239b9737994d27d31dc3bf878fc155a915471a
negativenagesh <gaonkarsub@gmail.com>
2025-08-12 22:09:50 +0530

    parsers

--- services/ingestion_service/parsers/__init__.py
+++ services/ingestion_service/parsers/__init__.py
@@ -1,5 +1,10 @@
 from .csv_parser import CSVParser
 from .xlsx_parser import XLSXParser, XLSXParserAdvanced
 from .base_parser import AsyncParser
+from .pdf_parser import PDFParser
+from .doc_parser import DOCParser
+from .docx_parser import DOCXParser
+from .odt_parser  import ODTParser
+from .text_parser import TextParser
 
-__all__ = ['CSVParser', 'XLSXParser', 'XLSXParserAdvanced', 'AsyncParser']
+__all__ = ['CSVParser', 'XLSXParser', 'XLSXParserAdvanced', 'AsyncParser', 'PDFParser', 'DOCParser', 'DOCXParser', 'ODTParser', 'TextParser']
